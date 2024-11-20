d9a41c294cbfe8a5e14c15a66d287a3079124142
ismaelbouzrouti <132908223+ismaelbouzrouti@users.noreply.github.com>
2024-11-20 19:38:36 +0100

    initial backend commit

---
+++ backend/art-api/src/test/java/org/enterpriseapp/artapi/ArtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enterpriseapp.artapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
